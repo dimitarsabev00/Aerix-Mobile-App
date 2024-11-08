ba824d93b79cbd0473949a64cbefe88aa21caf9e
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-11-08 10:29:08 +0200

    Added Bottom Tabs Design

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-300">
       <Text className="font-black">Aerix Mobile App</Text>
+      <Link href={"/home"}>Open Home Screen</Link>
       <StatusBar style="auto" />
     </View>
   );

8388931d0db51ca352e70a2fdb1fe6c954fa8c9f
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-11-08 10:02:37 +0200

    Added Fonts And Assets For Project

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-gray-300">
-      <Text>Aerix Mobile App</Text>
+      <Text className="font-black">Aerix Mobile App</Text>
       <StatusBar style="auto" />
     </View>
   );
